0c0eebd6d53f792f4d0eb2719b9a4fa0ce5682d8
Aditria Pardana <aditria.pardana20@gmail.com>
2022-07-21 14:32:08 +0700

    create HUD Display

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswer = false;
@@ -53,6 +55,8 @@ getNewQuestion = () => {
     return window.location.assign("/end.html");
   }
   questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTION}`;
+
   const quiestionIndex = Math.floor(Math.random() * availableQuestion.length);
   currentQuestion = availableQuestion[quiestionIndex];
   question.innerText = currentQuestion.question;
@@ -77,7 +81,10 @@ choices.map((choice) => {
     const classToApply =
       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-    console.log(classToApply);
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
     selectedChoice.parentElement.classList.add(classToApply);
 
     setTimeout(() => {
@@ -86,4 +93,10 @@ choices.map((choice) => {
     }, 1000);
   });
 });
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
+
 startGame();
