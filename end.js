eaf3a3685309d461ae41d9cfbe90eb096b0379a2
Aditria Pardana <aditria.pardana20@gmail.com>
2022-07-22 14:08:05 +0700

    Save Highscore in LocalStorage

--- end.js
+++ end.js
@@ -1,9 +1,13 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
-const finalScore = document.getElementById('finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
-finalScore.innerText = mostRecentScore
+const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
+
+const MAX_HIGH_SCORE = 5;
+finalScore.innerText = mostRecentScore;
+console.log(highScore);
 
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
@@ -12,4 +16,18 @@ username.addEventListener("keyup", () => {
 saveHighScore = (e) => {
   console.log(username.value);
   e.preventDefault();
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value,
+  };
+  highScore.push(score);
+  highScore.sort((a, b) => {
+    return b.score - a.score;
+  });
+  highScore.splice(5);
+
+  localStorage.setItem("highScore", JSON.stringify(highScore));
+  window.location.assign('/')
+  console.log(highScore);
 };
