2e5f7bdb1789d3d1460ed23f9cfec959bbf2a548
Aditria Pardana <aditria.pardana20@gmail.com>
2022-07-22 15:06:23 +0700

    update fetch api from triviaDB

--- game.js
+++ game.js
@@ -3,69 +3,91 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
-
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 let currentQuestion = {};
-let acceptingAnswer = false;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestion = [];
+let availableQuesions = [];
 
-let questionsArr = [];
-fetch("question.json")
+let questions = [];
+
+fetch(
+  "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple"
+)
   .then((res) => {
     return res.json();
   })
-  .then((loadedQuestion) => {
-    questionsArr = loadedQuestion;
+  .then((loadedQuestions) => {
+    questions = loadedQuestions.results.map((loadedQuestion) => {
+      const formattedQuestion = {
+        question: loadedQuestion.question,
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+
+      return formattedQuestion;
+    });
+
     startGame();
   })
   .catch((err) => {
-    console.log(err);
+    console.error(err);
   });
 
-//CONSTANT
+//CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTION = 3;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
   questionCounter = 0;
   score = 0;
-  availableQuestion = [...questionsArr];
-
+  availableQuesions = [...questions];
   getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
-  if (availableQuestion.length === 0 || questionCounter > MAX_QUESTION) {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
     //go to the end page
     return window.location.assign("/end.html");
   }
   questionCounter++;
-  progressText.innerText = `Question : ${questionCounter}/${MAX_QUESTION}`;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-  //Update the Progress Bar
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerHTML = currentQuestion.question;
 
-  progressBarFull.style.width = `${(questionCounter / MAX_QUESTION) * 100}%`;
-
-  const quiestionIndex = Math.floor(Math.random() * availableQuestion.length);
-  currentQuestion = availableQuestion[quiestionIndex];
-  question.innerText = currentQuestion.question;
-
-  choices.map((choice) => {
+  choices.forEach((choice) => {
     const number = choice.dataset["number"];
-    choice.innerText = currentQuestion["choice" + number];
+    choice.innerHTML = currentQuestion["choice" + number];
   });
 
-  availableQuestion.splice(quiestionIndex, 1);
-  acceptingAnswer = true;
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
 
-choices.map((choice) => {
+choices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
-    if (!acceptingAnswer) return;
+    if (!acceptingAnswers) return;
 
-    acceptingAnswer = false;
+    acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
